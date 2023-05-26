8103d46dc12c18bb001c726c23756d83a162b1b5
aligamalelgaml <ali.gamal.elgaml@gmail.com>
2023-05-26 23:14:31 +0300

    working avenger select and click, WIP: admin tools

--- js/app.js
+++ js/app.js
@@ -6,6 +6,19 @@ class Model {
         ]
 
         this.currentAvengerID = 1;
+
+        this.admin = false;
+    }
+
+    selectAvenger(id) {
+        this.currentAvengerID = id;
+        this.onAvengerChange(this.avengersList);
+    }
+
+    addClicks() {
+        this.avengersList[this.currentAvengerID - 1].clicks += 1;
+
+        this.onAvengerChange(this.avengersList);
     }
 
     /** Adds a new avenger, with a default number of clicks = 0.
@@ -22,6 +35,7 @@ class Model {
         }
 
         this.avengersList.push(newAvenger);
+        this.onAvengerChange(this.avengersList);
     }
 
     /** Edits an avenger's attributes.
@@ -34,15 +48,39 @@ class Model {
         this.avengersList.map((avenger) => {
             avenger.id === id ? {id: avenger.id, name: newName, url: newUrl, clicks: newClicks} : avenger
         });
+        this.onAvengerChange(this.avengersList);
     }
 
     deleteAvenger(id) {
         this.avengersList.filter((avenger) => avenger.id !== id);
+        this.onAvengerChange(this.avengersList);
+    }
+
+    toggleAdmin() {
+        if(this.admin) {
+            this.admin = false;
+        } else {
+            this.admin = true;
+        }
+
+        this.onAvengerChange(this.avengersList);
+    }
+
+    bindAvengerChanged(callback) {
+        this.onAvengerChange = callback;
     }
 }
 
 class View {
     constructor() {}
+
+    render(avenger, avengers) {
+        $("#thumbnail-col, #current-avenger").empty();
+        this.renderCurrentAvenger(avenger);
+        this.renderThumbnails(avengers);
+    }
+
+    // ======================
 
     renderAdminPanel() {
         const adminTemplate = this._createAdminTemplate();
@@ -50,24 +88,24 @@ class View {
     }
 
     _createAdminTemplate() {
-        template = `
-        <form id="admin-tools">
+        const template = `
+        <form id="admin-tools" class="bg-light p-4">
             <h2>Admin Tools</h2>
             <hr>
 
             <div class="mb-3">
                 <label for="avenger-name" class="form-label">Avenger Name:</label>
-                <input type="text" class="form-control" id="avenger-name">
+                <input type="text" class="form-control" id="admin-avenger-name">
             </div>
 
             <div class="mb-3">
                     <label for="avenger-url" class="form-label">Avenger URL:</label>
-                    <input type="text" class="form-control" id="avenger-url">
+                    <input type="text" class="form-control" id="admin-avenger-url">
             </div>
 
             <div class="mb-3">
                 <label for="avenger-clicks" class="form-label">Avenger Clicks:</label>
-                <input type="" class="form-control" id="avenger-clicks">
+                <input type="" class="form-control" id="admin-avenger-clicks">
             </div>
 
             <div class="buttons d-flex justify-content-between">
@@ -83,6 +121,7 @@ class View {
     // ====================
 
     renderCurrentAvenger(avenger) {
+        console.log(avenger);
         const avengerTemplate = this._createAvengerTemplate(avenger);
         $("#current-avenger").append(avengerTemplate);
     }
@@ -94,12 +133,11 @@ class View {
         </div>
 
         <div class="row">
-            <img src="${avenger.url}" class="img-fluid" alt="an image of a ${avenger.name}">
+            <img src="${avenger.url}" class="img-fluid p-5" id="avenger-img" alt="an image of a ${avenger.name}">
         </div>
 
-        <div class="row">
-            <span>Clicks: </span>
-            <span>${avenger.clicks}</span>
+        <div class="row text-center fs-2">
+            <span>Clicks: ${avenger.clicks}</span>
         </div>
         `;
 
@@ -110,17 +148,61 @@ class View {
 
     renderThumbnails(avengers) {
         avengers.forEach(avenger => {
-            const thumbnailTemplate = this._createAvengerThumbnailTemplate(url);
+            const thumbnailTemplate = this._createAvengerThumbnailTemplate(avenger.id, avenger.name, avenger.url);
             $("#thumbnail-col").append(thumbnailTemplate);
         });
     }
 
-    _createAvengerThumbnailTemplate(url) {
+    _createAvengerThumbnailTemplate(id, name, url) {
         const template = `
-        <img src="${url}" class="img-fluid mt-3" alt="">
+        <img src="${url}" class="img-fluid mt-3 thumbnail-img" id="${id}" alt="an image of ${name}">
         `;
 
         return template;
+    }
+
+    // ==================================
+
+    _resetForm() {
+        $("#admin-tools").reset();
+    }
+
+    // ==================================
+
+    bindSelectAvenger(handler) {
+        $("#thumbnail-col").on("click", ".thumbnail-img", function(e) {
+            e.preventDefault();
+
+            handler($(e.target).attr("id"));
+        });
+    }
+
+    bindClickAvenger(handler) {
+        $("#current-avenger").on("click", "#avenger-img", function (e) {
+            e.preventDefault();
+
+            handler();
+        });
+    }
+
+    bindAddAvenger(handler) {
+
+    }
+      
+    bindDeleteAvenger(handler) {
+
+    }
+      
+    bindToggleAdmin(handler) {
+        $("#admin-btn").click(function (e) { 
+            e.preventDefault();
+
+            $("#admin-toggler").toggleClass("d-none");
+            $("#admin-panel").toggleClass("d-none");
+
+            handler();
+        });
+
     }
 }
 
@@ -128,6 +210,47 @@ class Controller {
     constructor(model, view) {
         this.model = model;
         this.view = view;
+
+        this.view.render(this.model.avengersList[this.model.currentAvengerID - 1], this.model.avengersList); // Page initialization
+
+        this.model.bindAvengerChanged(this.onAvengerChange);
+
+        this.view.bindSelectAvenger(this.handleSelectAvenger);
+        this.view.bindClickAvenger(this.handleAddClick);
+        this.view.bindToggleAdmin(this.handleToggleAdmin);
+    }
+
+    onAvengerChange = (avengers) => {
+        this.view.render(avengers[this.model.currentAvengerID - 1], avengers);
+
+        if(this.model.admin) {
+            console.log("creating admin panel");
+            this.view.renderAdminPanel();
+        }
+    }
+
+    handleSelectAvenger = (id) => {
+        this.model.selectAvenger(id);
+    }
+
+    handleAddClick = () => {
+        this.model.addClicks();
+    }
+
+    handleAddAvenger = (avenger) => {
+        this.model.addAvenger(avenger.name, avenger.url, avenger.clicks);
+    }
+      
+    handleEditAvenger = (avenger) => {
+        this.model.editAvenger(avenger.id, avenger.name, avenger.url, avenger.clicks)
+    }
+      
+    handleDeleteAvenger = (avenger) => {
+        this.model.deleteAvenger(avenger.id)
+    }
+      
+    handleToggleAdmin = () => {
+        this.model.toggleAdmin()
     }
 }
 
